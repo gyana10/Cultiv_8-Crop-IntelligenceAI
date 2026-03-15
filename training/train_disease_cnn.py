c8821b259a7af0248d4af0121a8e232bb6857128
Gyana Ranjan Mohanty <gyanarmohanty20@gmail.com>
2026-03-15 12:52:23 +0530

    traing data settings

--- training/train_disease_cnn.py
+++ training/train_disease_cnn.py
@@ -8,3 +8,10 @@ IMG_SIZE=224
 BATCH_SIZE=32
 EPOCHS=10
 DATASET_PATH="../dataset/plant_disease"
+
+train_datagen=ImageDataGenerator(rescale=1.0/255,validation_split=0.2,rotation_range=20,
+                                 zoom_range=0.2,horizontal_flip=True)
+
+train_data=train_datagen.flow_from_directory(DATASET_PATH,target_size=(IMG_SIZE,IMG_SIZE),
+                                        batch_size=BATCH_SIZE,class_mode='categorical',
+                                        subset='training')
