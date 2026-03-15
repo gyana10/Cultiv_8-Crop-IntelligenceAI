6f005bb344df644bc72c1bb75a63a07eaca61515
Gyana Ranjan Mohanty <gyanarmohanty20@gmail.com>
2026-03-15 12:29:46 +0530

    Added CNN training script and imported required libraries

---
+++ training/train_disease_cnn.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D,Flatten,Dense,Dropout
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam
+
