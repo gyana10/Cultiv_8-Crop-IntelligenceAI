be7ca2860c409cb261872ff1100c063baacd232a
Gyana Ranjan Mohanty <gyanarmohanty20@gmail.com>
2026-03-15 12:36:07 +0530

    setting Hyperparameters

--- training/train_disease_cnn.py
+++ training/train_disease_cnn.py
@@ -4,3 +4,7 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D,Flatten,Dense,Dropout
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import Adam
 
+IMG_SIZE=224
+BATCH_SIZE=32
+EPOCHS=10
+DATASET_PATH="../dataset/plant_disease"
